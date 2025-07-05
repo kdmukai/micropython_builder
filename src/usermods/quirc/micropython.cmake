22f1c11191d1a6a8b9b45635fba7cac3561840f7
kdmukai <kdmukai@gmail.com>
2025-07-05 16:55:14 -0500

    quirc float C_FLAG optimization

--- src/usermods/quirc/micropython.cmake
+++ src/usermods/quirc/micropython.cmake
@@ -1,7 +1,7 @@
-# # Create an INTERFACE library for our C module.
+# Create an INTERFACE library for our C module.
 add_library(quirc INTERFACE)
 
-# # Add our source files to the lib
+# Add our source files to the lib
 target_sources(quirc INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/quirc/lib/decode.c
     ${CMAKE_CURRENT_LIST_DIR}/quirc/lib/identify.c
@@ -10,25 +10,22 @@ target_sources(quirc INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/quirc_bindings.c
  )
 
-# # Add the current directory as an include directory.
+# Add the current directory as an include directory.
 target_include_directories(quirc INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}
 )
 
-target_compile_definitions(quirc INTERFACE)
+target_compile_definitions(quirc INTERFACE
+    # force quirc to use single precision floating point math
+    -DQUIRC_FLOAT_TYPE=float
+    -DQUIRC_USE_TGMATH=1
+)
 
-# # Be sure to set the -O2 "optimize" flag!!
-# target_compile_options(quirc INTERFACE
-#     -Wno-unused-variable
-#     -Wno-sign-compare
-#     -Wno-maybe-uninitialized
-#     -O2
+# Be sure to set the -O2 "optimize" flag!!
+target_compile_options(quirc INTERFACE
+    -O2
+)
 
-#     # force quirc to use single precision floating point math
-#     -DQUIRC_FLOAT_TYPE=float
-#     -DQUIRC_USE_TGMATH
-# )
-
-# # Link our INTERFACE library to the usermod target.
+# Link our INTERFACE library to the usermod target.
 target_link_libraries(usermod INTERFACE quirc)
 
